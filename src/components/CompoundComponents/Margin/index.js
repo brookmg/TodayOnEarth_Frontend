40a544ccb4a0232c6b1ad99915f197c60ad86ee8
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-07 17:53:53 +0300

    ui: add Margin component for maintaining cleaner code on components

---
+++ src/components/CompoundComponents/Margin/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+/* 
+This should help keep the code clean by setting margin for 
+every single child of this component
+*/
+
+const Margin = (props) => {
+
+    const marginVertical = props.vertical;
+    const marginHorizontal = props.horizontal;
+
+
+    const marginTop = props.top || marginVertical;
+    const marginBottom = props.bottom || marginVertical;
+    const marginRight = props.right || marginHorizontal;
+    const marginLeft = props.left || marginHorizontal;
+
+
+
+    const childrenWithMargin = React.Children.map(props.children, child =>
+        React.cloneElement(child, {
+            style: {
+                ...child.props.style,
+
+                margin: props.all,
+
+                marginTop,
+                marginBottom,
+                marginRight,
+                marginLeft,
+            }
+        })
+    );
+
+    return (
+        <>
+            {childrenWithMargin}
+        </>
+    )
+
+}
+
+export default Margin;
