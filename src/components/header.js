a2305b0b21db0dba892466e0c06feba0c028648a
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-07 17:50:35 +0300

    ui: create themeable header with fading searchbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,113 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { FormInput, Fade } from "shards-react";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+
+import ThemePalletteContext from "./Contexts/ThemePalletteContext"
+import AnchorButton from "./UIElements/AnchorButton"
+
+
+const Header = ({ siteTitle }) => {
+  const handleSearchButtonClick = (e) => {
+    if (searchBarText === "") {
+      searchBarRef.current.focus()
+    } else {
+      handleSearchSubmit()
+    }
+
+  }
+
+  const handleSearchButtonMouseEnter = (e) => {
+    setIsSearchBarShowing(true)
+  }
+
+
+  const handleSearchBarChange = (e) => {
+    const value = e.target.value
+    setSearchBarText(value)
+  }
+  const handleSearchBarKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearchSubmit()
+    }
+  }
+  const handleSearchSubmit = (e) => {
+    if (searchBarText !== "") {
+      console.log("searching for:", searchBarText)
+      window.location.href=`/mobile/s?search_term=${encodeURIComponent(searchBarText)}`
+    }
+  }
+
+
+  const theme = React.useContext(ThemePalletteContext)
+  const [isSearchBarShowing, setIsSearchBarShowing] = React.useState(false);
+  const [searchBarText, setSearchBarText] = React.useState("");
+  let searchBarRef = React.createRef();
+
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <div style={{
+          display: 'flex'
+        }}>
+
+          <h1 style={{ flex: 1, margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: theme.color_text,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+
+          <div style={{
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+            <Fade
+              onMouseEnter={handleSearchButtonMouseEnter}
+              in={isSearchBarShowing}>
+
+              <FormInput
+                innerRef={searchBarRef}
+
+                onChange={handleSearchBarChange}
+                onKeyPress={handleSearchBarKeyPress}
+
+                size="sm" placeholder="Search for keywords" />
+
+            </Fade>
+            <AnchorButton
+              onMouseEnter={handleSearchButtonMouseEnter}
+              onClick={handleSearchButtonClick}>
+              🔍
+              </AnchorButton>
+            <AnchorButton>≡</AnchorButton>
+          </div>
+
+
+        </div>
+
+
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
