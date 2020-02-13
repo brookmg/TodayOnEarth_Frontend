7b32fb46ed10c218ddf022ff1aaf2f0b4463ac76
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-13 17:23:27 +0300

    ui: export theme provider from context

--- src/components/Contexts/ThemePalletteContext/index.js
+++ src/components/Contexts/ThemePalletteContext/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ColorPalletteDefinition from "./ColorPalletteDefinition"
 
-const ThemePalletteContext = React.createContext(ColorPalletteDefinition)
+const ThemePalletteContext = React.createContext({
+    ...ColorPalletteDefinition,
+
+    setBackgroundColor: () => { },
+    setTextColor: () => { },
+    setFadedTextColor: () => { }
+})
+
+export const ThemeProvider = (props) => {
+    const [theme, setTheme] = React.useState({ ...ColorPalletteDefinition });
+
+    return (
+        <ThemePalletteContext.Provider value={{ ...theme, setTheme }}>
+            {props.children}
+        </ThemePalletteContext.Provider>
+    )
+}
 
 export default ThemePalletteContext;
