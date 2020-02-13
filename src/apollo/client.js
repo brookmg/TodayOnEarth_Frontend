c63c40dda93bfb9da1d0267ffcb8df23c0eaabeb
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-13 13:57:04 +0300

    gql: integrate apollo client that uses fragmentMatcher

---
+++ src/apollo/client.js
@@ -0,0 +1,14 @@
+import ApolloClient from 'apollo-boost';
+import fetch from 'isomorphic-fetch';
+import { IntrospectionFragmentMatcher, InMemoryCache } from 'apollo-cache-inmemory';
+import introspectionQueryResultData from './schemaQuery/fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+    introspectionQueryResultData
+});
+
+export const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    fetch,
+    cache: new InMemoryCache({ fragmentMatcher })
+});
