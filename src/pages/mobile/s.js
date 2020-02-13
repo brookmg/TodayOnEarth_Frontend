d5ee676fcc3d5453973e63f49de5fc1704b85858
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-13 14:07:17 +0300

    ui: add more search params and prepare for gql

--- src/pages/mobile/s.js
+++ src/pages/mobile/s.js
@@ -29,10 +29,41 @@ const AdvancedFiltersSection = (props) => {
         setCheckedItems({ ...checkedItems, [name]: !isChecked });
     }
 
+    const handleStartTimeChange = (e) => { setStartTime(e.target.value) }
+    const handleEndTimeChange = (e) => { setEndTime(e.target.value) }
+    const handleSearchFilterTextChange = (e) => { setFilterSearchBar(e.target.value) }
+
+    const handleRefineClick = (event) => {
+        const selectedCountries = `${Object.keys(checkedItems).filter(e => checkedItems[e])}`
+        const startTimeStamp = encodeURIComponent(startTime)
+        const endTimeStamp = encodeURIComponent(endTime)
+        const searchFilterSearchBarSafe = encodeURIComponent(searchFilterSearchBar || "")
+
+        const searchQuery = `expanded=1&locations=${selectedCountries}&start_time=${startTimeStamp}&end_time=${endTimeStamp}&search_term=${searchFilterSearchBarSafe}`
+        console.log(searchQuery)
+        window.open(`s?${searchQuery}`, "_self")
+    }
+
+    const convertDateToInputFormat = (d) => {
+        const date = new Date(d)
+        try {
+            return date.toISOString().split('T')[0]
+        }
+        catch (e) {
+            console.error(e)
+        }
+    }
     const locations = ['Africa', 'Europe', 'Asia',]
 
-    const [isAdvancedFiltersCollapsed, setAdvancedFiltersCollapsed] = React.useState(true);
-    const [checkedItems, setCheckedItems] = React.useState({});
+    const initialCheckedItems = {}
+    if (props.locations)
+        props.locations.split(',').forEach(e => initialCheckedItems[e] = true)
+
+    const [isAdvancedFiltersCollapsed, setAdvancedFiltersCollapsed] = React.useState(props.isAdvancedFilterCollapsed);
+    const [searchFilterSearchBar, setFilterSearchBar] = React.useState(props.searchTerm);
+    const [checkedItems, setCheckedItems] = React.useState(initialCheckedItems);
+    const [startTime, setStartTime] = React.useState(props.startTime);
+    const [endTime, setEndTime] = React.useState(props.endTime);
 
     return (
         <div>
@@ -47,6 +78,16 @@ const AdvancedFiltersSection = (props) => {
 
                     <Card>
                         <CardBody>
+                            <Margin bottom="1rem">
+
+                                <div>
+                                    <CardTitle>Including the words</CardTitle>
+                                    <div>
+                                        <FormInput value={searchFilterSearchBar} onChange={handleSearchFilterTextChange} size="sm" />
+                                    </div>
+
+                                </div>
+                            </Margin>
                             <Margin bottom="1rem">
 
                                 <div>
@@ -78,8 +119,8 @@ const AdvancedFiltersSection = (props) => {
                                     <CardTitle>Time Period</CardTitle>
                                     <div>
 
-                                        Starting From: <FormInput type="date" size="sm" />
-                                        Ending At: <FormInput type="date" size="sm" />
+                                        Starting From: <FormInput value={convertDateToInputFormat(startTime)} onChange={handleStartTimeChange} type="date" size="sm" />
+                                        Ending At: <FormInput value={convertDateToInputFormat(endTime)} onChange={handleEndTimeChange} type="date" size="sm" />
 
                                     </div>
 
@@ -87,7 +128,7 @@ const AdvancedFiltersSection = (props) => {
                             </Margin>
                             <div>
                                 <Margin vertical="1rem">
-                                    <Button theme="success">Refine search</Button>
+                                    <Button onClick={handleRefineClick} theme="success">Refine search</Button>
                                 </Margin>
                             </div>
                         </CardBody>
@@ -101,18 +142,28 @@ const AdvancedFiltersSection = (props) => {
 
 }
 
-const SearchPage = withQueryParsedURL((props) => (
-    <Layout>
-        <SEO title="Home" />
+const SearchPage = withQueryParsedURL((props) => {
+    const queryParsedURL = props.queryParsedURL
+    return (
+        <Layout>
+            <SEO title="Home" />
 
-        <AdvancedFiltersSection />
+            <AdvancedFiltersSection
+                searchTerm={queryParsedURL.search_term}
+                locations={queryParsedURL.locations}
+                startTime={queryParsedURL.start_time}
+                endTime={queryParsedURL.end_time}
+                isAdvancedFilterCollapsed={!queryParsedURL.expanded}
+            />
 
-        <h2>Search results for: "{props.queryParsedURL.search_term}"</h2>
-        <PostHolderCard />
+            <Margin top="1em">
+                <h2>Search results for: "{queryParsedURL.search_term}"</h2>
+                <PostHolderCard />
+            </Margin>
 
-
-        <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
-))
+            <Link to="/page-2/">Go to page 2</Link>
+        </Layout>
+    )
+})
 
 export default SearchPage
