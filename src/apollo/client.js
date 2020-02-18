1c203f876b83d1eead09a6d8ef35a17099126047
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-18 19:02:02 +0300

    apollo: send cookies along with every request

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,6 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 import { IntrospectionFragmentMatcher, InMemoryCache } from 'apollo-cache-inmemory';
 import introspectionQueryResultData from './schemaQuery/fragmentTypes.json';
-import { getToken } from '../services/auth.js';
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
     introspectionQueryResultData
@@ -12,15 +11,11 @@ export const client = new ApolloClient({
     uri: 'http://localhost:4000/',
     fetch,
     cache: new InMemoryCache({ fragmentMatcher }),
-    request: (operation) => {
-        const token = getToken()
-        if (token && token.length > 0) {
-            operation.setContext({
-                headers: {
-                    Authorization: token
-                }
-            })
-        }
-
+    request: async operation => {
+        operation.setContext({
+            fetchOptions: {
+                credentials: 'include'
+            }
+        })
     }
 });
