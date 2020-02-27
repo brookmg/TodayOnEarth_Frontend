fd279f8c73dfa66bf3dbfa90f58f975ffb310f68
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-27 20:42:15 +0300

    theme: get from localStorage if available

--- src/components/Contexts/ThemePalletteContext/index.js
+++ src/components/Contexts/ThemePalletteContext/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import DefaultThemeDefinition from "./DefaultThemeDefinition"
 
+const localStorageTheme = JSON.parse(localStorage.getItem("theme") || "{}")
+
+// This will give precedence to local storage theme over default theme
+const themeToUse = { ...ColorPalletteDefinition, ...localStorageTheme }
+
 const ThemePalletteContext = React.createContext({
-    ...DefaultThemeDefinition,
+    ...themeToUse,
     setTheme: () => { }
 })
 
 export const ThemeProvider = (props) => {
-    const [theme, setTheme] = React.useState({ ...DefaultThemeDefinition });
+    const [theme, setTheme] = React.useState({ ...themeToUse });
 
     return (
         <ThemePalletteContext.Provider value={{ ...theme, setTheme }}>
