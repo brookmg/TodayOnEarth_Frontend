3c3be3b1a3a7c1399d560c83326df3186e4e4d46
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-02 13:47:20 +0300

    posts: make link parser component

---
+++ src/components/UIElements/ParseLinks.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AnchorButton from './AnchorButton';
+
+const hashTagRegex = /\B(\#[a-zA-Z]+\b)/gi;
+
+const ParseLinks = (props) => {
+    if (!props.children) return null
+
+    const urlRegex = /(\w+\.[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+)/gi;
+    const parts = props.children.split(urlRegex);
+    const urlParts = parts
+    // parse urls
+    for (let i = 1; i < urlParts.length; i += 2) {
+        const url = urlParts[i];
+        const splitUrl = url.split("://");
+        const cleanUrlLeft = splitUrl[0];
+        const cleanUrlRight = splitUrl[1];
+        const cleanUrl = cleanUrlRight ? cleanUrlRight : cleanUrlLeft;
+        urlParts[i] = <span> {cleanUrlRight && `${cleanUrlLeft}://`}
+            <AnchorButton key={i} url={`http://${cleanUrl}`}>{cleanUrl}</AnchorButton>;
+        </span>;
+    }
+
+    return <>{parts}</>;
+};
+export default ParseLinks;
