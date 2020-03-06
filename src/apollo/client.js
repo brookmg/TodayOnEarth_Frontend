fb2adc185e3bc60233b61224d3740a76bff85880
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-06 13:37:28 +0300

    apollo: add support for websockets

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,21 +1,52 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import fetch from 'isomorphic-fetch';
 import { IntrospectionFragmentMatcher, InMemoryCache } from 'apollo-cache-inmemory';
 import introspectionQueryResultData from './schemaQuery/fragmentTypes.json';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import ws from "ws"
+import { isBrowser } from '../utils/index.js';
+
+const HTTP_LINK_URI = `http://localhost:3400/graphql`
+const WS_LINK_URI = `ws://localhost:3400/graphql`
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
     introspectionQueryResultData
 });
 
-export const client = new ApolloClient({
-    uri: 'http://localhost:3400/graphql',
+const fetchOptions = {
+    credentials: 'include'
+}
+
+const httpLink = new HttpLink({
+    uri: HTTP_LINK_URI,
     fetch,
+    fetchOptions
+});
+
+const wsForNode = isBrowser() ? null : ws
+
+const wsLink = new WebSocketLink({
+    uri: WS_LINK_URI,
+    options: {
+        reconnect: true
+    },
+    webSocketImpl: wsForNode
+});
+
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+)
+
+export const client = new ApolloClient({
+    link,
     cache: new InMemoryCache({ fragmentMatcher }),
-    request: async operation => {
-        operation.setContext({
-            fetchOptions: {
-                credentials: 'include'
-            }
-        })
-    }
 });
