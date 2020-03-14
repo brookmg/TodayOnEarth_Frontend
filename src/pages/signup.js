1f3128e94816099a9630dc09290c9fe8e5ef14f0
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 11:49:02 +0300

    signup: add desktop layout

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -11,8 +11,13 @@ import Margin from "../components/CompoundComponents/Margin"
 import { FormInput, Button } from "shards-react"
 import AnchorButton from "../components/UIElements/AnchorButton"
 import ButtonSuccess from "../components/UIElements/ButtonSuccess"
+import { useMediaQuery } from 'react-responsive'
+
 
 const SignUp = ({ email, first_name, last_name, username, google_id, facebook_id, twitter_id, github_id, linkedin_id, telegram_id }) => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+  })
   const auth = React.useContext(AuthContext)
   const [user, setUser] = React.useState({ email, first_name, last_name, username, google_id, facebook_id, twitter_id, github_id, linkedin_id, telegram_id });
 
@@ -43,95 +48,98 @@ const SignUp = ({ email, first_name, last_name, username, google_id, facebook_id
           handleSubmit(event)
         }}
       >
+        <div style={{ display: 'flex', flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
+          <div style={{ flex: 1 }}>
+            <Margin horizontal="0.5em" vertical="0.25em">
+              <ButtonSignInWith
+                url={`${authEndpoint}/twitter`}
+                borderColor={"#49a0e9"}
+                backgroundColor={"#fff"}
+                color={"#49a0e9"}
+                imgSrc={`https://abs.twimg.com/favicons/twitter.ico`}
+                value="Fill with Twitter"
+              />
 
-        <label>
-          Email<br />
-          <FormInput name="email" value={user.email} onChange={handleUpdate} />
-        </label>
-        <br />
+              <ButtonSignInWith
+                url={`${authEndpoint}/facebook`}
+                borderColor={"#3b5998"}
+                backgroundColor={"#3b5998"}
+                color={"#fff"}
+                imgSrc={`https://upload.wikimedia.org/wikipedia/commons/4/4d/F_icon_reversed.svg`}
+                value="Fill with Facebook"
+              />
 
-        <label>
-          First Name<br />
-          <FormInput name="first_name" value={user.first_name} onChange={handleUpdate} />
-        </label>
-        <br />
+              <ButtonSignInWith
+                url={`${authEndpoint}/google`}
+                borderColor={"#eee"}
+                backgroundColor={"#fff"}
+                color={"#000"}
+                imgSrc={`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_"G"_Logo.svg`}
+                value="Fill with Google"
+              />
 
-        <label>
-          Last Name<br />
-          <FormInput name="last_name" value={user.last_name} onChange={handleUpdate} />
-        </label>
-        <br />
+              <ButtonSignInWith
+                url={`${authEndpoint}/github`}
+                borderColor={"#24292e"}
+                backgroundColor={"#fff"}
+                color={"#24292e"}
+                imgSrc={`https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg`}
+                value="Fill with GitHub"
+              />
 
-        <label>
-          Username<br />
-          <FormInput name="username" value={user.username} onChange={handleUpdate} />
-        </label>
-        <br />
+              <ButtonSignInWith
+                url={`${authEndpoint}/linkedin`}
+                borderColor={"#3577b5"}
+                backgroundColor={"#3577b5"}
+                color={"#fff"}
+                imgSrc={`https://static-exp1.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico`}
+                value="Fill with LinkedIn"
+              />
+            </Margin>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div>
+              Email
+              <FormInput name="email" value={user.email} onChange={handleUpdate} />
+            </div>
+            
 
-        <label>
-          Password<br />
-          <FormInput
-            type="password"
-            name="password"
-            onChange={handleUpdate}
-          />
-        </label>
-        <br />
+            <div>
+              First Name
+              <FormInput name="first_name" value={user.first_name} onChange={handleUpdate} />
+            </div>
+            
 
-        <Margin right="0.5em">
-          <ButtonSignInWith
-            url={`${authEndpoint}/twitter`}
-            borderColor={"#49a0e9"}
-            backgroundColor={"#fff"}
-            color={"#49a0e9"}
-            imgSrc={`https://abs.twimg.com/favicons/twitter.ico`}
-            value="Fill with Twitter"
-          />
+            <div>
+              Last Name
+              <FormInput name="last_name" value={user.last_name} onChange={handleUpdate} />
+            </div>
+            
 
-          <ButtonSignInWith
-            url={`${authEndpoint}/facebook`}
-            borderColor={"#3b5998"}
-            backgroundColor={"#3b5998"}
-            color={"#fff"}
-            imgSrc={`https://upload.wikimedia.org/wikipedia/commons/4/4d/F_icon_reversed.svg`}
-            value="Fill with Facebook"
-          />
+            <div>
+              Username
+              <FormInput name="username" value={user.username} onChange={handleUpdate} />
+            </div>
+            
 
-          <ButtonSignInWith
-            url={`${authEndpoint}/google`}
-            borderColor={"#eee"}
-            backgroundColor={"#fff"}
-            color={"#000"}
-            imgSrc={`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_"G"_Logo.svg`}
-            value="Fill with Google"
-          />
-         
-          <ButtonSignInWith
-            url={`${authEndpoint}/github`}
-            borderColor={"#24292e"}
-            backgroundColor={"#fff"}
-            color={"#24292e"}
-            imgSrc={`https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg`}
-            value="Fill with GitHub"
-          />
+            <div>
+              Password
+              <FormInput
+                type="password"
+                name="password"
+                onChange={handleUpdate}
+              />
+            </div>
+            
+          </div>
+        </div>
 
-          <ButtonSignInWith
-            url={`${authEndpoint}/linkedin`}
-            borderColor={"#3577b5"}
-            backgroundColor={"#3577b5"}
-            color={"#fff"}
-            imgSrc={`https://static-exp1.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico`}
-            value="Fill with LinkedIn"
-          />
-        </Margin>
-
-        <br />
+        
         <Margin vertical="1em" right="1em">
           <ButtonSuccess type="submit" >Sign Up</ButtonSuccess><br />
           <AnchorButton url="/app/login" onClick={handleSignIn}>Already have an account? Log in here</AnchorButton>
         </Margin>
       </form>
-
     </>
   )
 }
