87a1becd5fd625f6fbb30ba96089e060adbf2140
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 17:59:48 +0300

    BackgroundImagePlaceHolder: convert to styled components

--- src/components/BackgroundImagePlaceHolder.js
+++ src/components/BackgroundImagePlaceHolder.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ContentLoader from 'react-content-loader';
+import styled from "styled-components";
 
+
+const StyledContentLoader = styled(ContentLoader)`
+    min-width: 100%;
+`
 
 const BackgroundImagePlaceHolder = () => <div>
-    <ContentLoader height={200} style={{
-        minWidth: '100%',
-    }}>
+    <StyledContentLoader height={200}>
         <rect x="0" y="0" width="100%" height="100%" />
-    </ContentLoader>
+    </StyledContentLoader>
 </div>;
 
 export default BackgroundImagePlaceHolder
