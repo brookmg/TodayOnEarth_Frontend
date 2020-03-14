9dffbc409e40c6e025fce8ecfebfb4e9fb8704e0
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 20:35:16 +0300

    index: convert to styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,35 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import TrendingKeywords from "../components/TrendingKeywords";
 import PostsTrendingToday from "../components/PostsTrendingToday";
+import styled from "styled-components";
 
+
+const StyledDiv = styled.div`
+    flex: 1;
+    paddingTop: 15em;
+    height: 100vh;
+    overflow: auto;
+`
 
 const IndexPage = () => {
-  return (
-    <Layout
-      rightSideDesktopComponent={(
-        <div style={{
-          flex: 1,
-          paddingTop: '15em',
-          height: '100vh',
-          overflow: 'auto'
-        }}
-        >
-          <TrendingKeywords />
-        </div>
-      )}
-      render={
-        ({ scrollValue, height }) => {
-          return (
-            <>
-              <SEO title="Home" />
-              <PostsTrendingToday scrollValue={scrollValue} height={height} />
-            </>
-          )
-        }}>
-    </Layout>
-  )
+    return (
+        <Layout
+            rightSideDesktopComponent={(
+                <StyledDiv>
+                    <TrendingKeywords />
+                </StyledDiv>
+            )}
+            render={
+                ({ scrollValue, height }) => {
+                    return (
+                        <>
+                            <SEO title="Home" />
+                            <PostsTrendingToday scrollValue={scrollValue} height={height} />
+                        </>
+                    )
+                }}>
+        </Layout>
+    )
 }
 
 export default IndexPage
