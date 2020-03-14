fe286082ccbc3d01e1e4c61b43e868859559092d
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 20:17:54 +0300

    SignIn: convert to styled components

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,108 +8,119 @@ import AnchorButton from "./UIElements/AnchorButton"
 import ButtonSignInWith from "./UIElements/ButtonSignInWith"
 import ButtonSuccess from "./UIElements/ButtonSuccess"
 import ScreenSizeContext from "./Contexts/ScreenSizeContext"
+import styled from "styled-components"
 
 
-const SignIn = (props) => {
-  const isDesktopOrLaptop = React.useContext(ScreenSizeContext).isDesktopOrLaptop
+const StyledDisplayFlexDiv = styled.div`
+    display: flex;
+`
 
-  const auth = React.useContext(AuthContext)
-  const [user, setUser] = React.useState({})
+const StyledForm = styled.form`
+    flex: 1;
+`
 
-  const handleUpdate = event =>
-    setUser({ ...user, [event.target.name]: event.target.value, })
+const StyledFlex1MarginDiv = styled.div`
+    flex: 1;
+    margin-left: 0.5em;
+`
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    handleSignIn(user)
-      .then(() => auth.refreshActiveUser(
-        () => navigate(`/app/profile`)
-      ))
-      .catch(e => alert(e.message))
-  }
-  const handleSignUp = () => { navigate(`/signup`) }
+const SignIn = () => {
+    const isDesktopOrLaptop = React.useContext(ScreenSizeContext).isDesktopOrLaptop
 
-  if (isLoggedIn()) {
-    navigate(`/app/profile`)
-  }
+    const auth = React.useContext(AuthContext)
+    const [user, setUser] = React.useState({})
 
-  const authEndpoint = process.env.GATSBY_AUTH_ENDPOINT
+    const handleUpdate = event =>
+        setUser({ ...user, [event.target.name]: event.target.value, })
 
-  return (
-    <>
-      <h1>Log in</h1>
-      <div style={{ display: 'flex', flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
-        <form
-          style={{ flex: 1 }}
-          method="post"
-          onSubmit={event => handleSubmit(event)
-          }
-        >
-          <Margin vertical="0.5em" horizontal="0.5em">
+    const handleSubmit = event => {
+        event.preventDefault()
+        handleSignIn(user)
+            .then(() => auth.refreshActiveUser(
+                () => navigate(`/app/profile`)
+            ))
+            .catch(e => alert(e.message))
+    }
+    const handleSignUp = () => { navigate(`/signup`) }
 
-            <FormInput placeholder="Email" name="email" onChange={handleUpdate} />
+    if (isLoggedIn()) {
+        navigate(`/app/profile`)
+    }
 
-            <FormInput
-              placeholder="Password"
-              type="password"
-              name="password"
-              onChange={handleUpdate}
-            />
-            <ButtonSuccess type="submit"> Log In </ButtonSuccess>
-            <AnchorButton url="/signup" onClick={handleSignUp}>Dont have an account? Sign up here</AnchorButton>
+    const authEndpoint = process.env.GATSBY_AUTH_ENDPOINT
 
-          </Margin>
-        </form>
-        <div style={{ flex: 1, marginLeft: "0.5em" }}>
-          <Margin horizontal="0.5em" vertical="0.25em">
-            <ButtonSignInWith
-              url={`${authEndpoint}/twitter`}
-              borderColor={"#49a0e9"}
-              backgroundColor={"#fff"}
-              color={"#49a0e9"}
-              imgSrc={`https://abs.twimg.com/favicons/twitter.ico`}
-              value="Sign in with Twitter"
-            />
+    return (
+        <>
+            <h1>Log in</h1>
+            <StyledDisplayFlexDiv style={{ flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
+                <StyledForm
+                    method="post"
+                    onSubmit={event => handleSubmit(event)
+                    }
+                >
+                    <Margin vertical="0.5em" horizontal="0.5em">
+                        <FormInput placeholder="Email" name="email" onChange={handleUpdate} />
+                        <FormInput
+                            placeholder="Password"
+                            type="password"
+                            name="password"
+                            onChange={handleUpdate}
+                        />
+                        <ButtonSuccess type="submit"> Log In </ButtonSuccess>
+                        <AnchorButton url="/signup" onClick={handleSignUp}>Dont have an account? Sign up here</AnchorButton>
 
-            <ButtonSignInWith
-              url={`${authEndpoint}/facebook`}
-              borderColor={"#3b5998"}
-              backgroundColor={"#3b5998"}
-              color={"#fff"}
-              imgSrc={`https://upload.wikimedia.org/wikipedia/commons/4/4d/F_icon_reversed.svg`}
-              value="Sign in with Facebook"
-            />
+                    </Margin>
+                </StyledForm>
+                <StyledFlex1MarginDiv>
+                    <Margin horizontal="0.5em" vertical="0.25em">
+                        <ButtonSignInWith
+                            url={`${authEndpoint}/twitter`}
+                            borderColor={"#49a0e9"}
+                            backgroundColor={"#fff"}
+                            color={"#49a0e9"}
+                            imgSrc={`https://abs.twimg.com/favicons/twitter.ico`}
+                            value="Sign in with Twitter"
+                        />
 
-            <ButtonSignInWith
-              url={`${authEndpoint}/google`}
-              borderColor={"#eee"}
-              backgroundColor={"#fff"}
-              color={"#000"}
-              imgSrc={`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_"G"_Logo.svg`}
-              value="Sign in with Google"
-            />
-            <ButtonSignInWith
-              url={`${authEndpoint}/github`}
-              borderColor={"#24292e"}
-              backgroundColor={"#fff"}
-              color={"#24292e"}
-              imgSrc={`https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg`}
-              value="Sign in with GitHub"
-            />
+                        <ButtonSignInWith
+                            url={`${authEndpoint}/facebook`}
+                            borderColor={"#3b5998"}
+                            backgroundColor={"#3b5998"}
+                            color={"#fff"}
+                            imgSrc={`https://upload.wikimedia.org/wikipedia/commons/4/4d/F_icon_reversed.svg`}
+                            value="Sign in with Facebook"
+                        />
 
-            <ButtonSignInWith
-              url={`${authEndpoint}/linkedin`}
-              borderColor={"#3577b5"}
-              backgroundColor={"#3577b5"}
-              color={"#fff"}
-              imgSrc={`https://static-exp1.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico`}
-              value="Sign in with LinkedIn"
-            />
-          </Margin>
-        </div>
-      </div>
-    </>
-  )
+                        <ButtonSignInWith
+                            url={`${authEndpoint}/google`}
+                            borderColor={"#eee"}
+                            backgroundColor={"#fff"}
+                            color={"#000"}
+                            imgSrc={`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_"G"_Logo.svg`}
+                            value="Sign in with Google"
+                        />
+                        <ButtonSignInWith
+                            url={`${authEndpoint}/github`}
+                            borderColor={"#24292e"}
+                            backgroundColor={"#fff"}
+                            color={"#24292e"}
+                            imgSrc={`https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg`}
+                            value="Sign in with GitHub"
+                        />
+
+                        <ButtonSignInWith
+                            url={`${authEndpoint}/linkedin`}
+                            borderColor={"#3577b5"}
+                            backgroundColor={"#3577b5"}
+                            color={"#fff"}
+                            imgSrc={`https://static-exp1.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico`}
+                            value="Sign in with LinkedIn"
+                        />
+                    </Margin>
+                </StyledFlex1MarginDiv>
+            </StyledDisplayFlexDiv>
+        </>
+    )
 
 }
 export default SignIn
