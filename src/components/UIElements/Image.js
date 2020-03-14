77a6c4707ac19752fc26132083b20b0dbef25bfb
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 17:46:02 +0300

    Image: refactor and convert to styled components

--- src/components/UIElements/Image.js
+++ src/components/UIElements/Image.js
@@ -1,28 +1,15 @@
 import React from "react"
-import ContentLoader from 'react-content-loader'
+import ImagePlaceHolder from "./ImagePlaceHolder"
+import styled from "styled-components"
 
-const ImagePlaceHolder = (props) =>
-    <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        ...props.style
-    }}>
-        <ContentLoader style={{
-            minWidth: '100%',
-            minHeight: '100%',
-        }}
-            title={props.isErrorFound && "Error Loading"}
-            animate={props.isErrorFound}
-            speed={5}
-        >
-            <rect x="0" y="0" width="100%" height="100%" />
-        </ContentLoader>
-        <p style={{
-            position: "absolute",
-        }}>Could not load image</p>
-    </div>
 
+const StyledImg = styled.img`
+    object-fit: contain;
+    max-width: 100%;
+    max-height: 100%;
+    width: auto;
+    height: auto;
+`
 
 const Image = (props) => {
     const handleImageErrored = () => setIsImageLoadedError(true)
@@ -30,15 +17,7 @@ const Image = (props) => {
 
     return (
         props.src && !isImageLoadedError ?
-            <img
-                style={{
-                    objectFit: 'contain',
-                    maxWidth: '100%',
-                    maxHeight: '100%',
-                    width: 'auto',
-                    height: 'auto',
-                    ...props.style
-                }}
+            <StyledImg
                 height={100}
                 width={100}
                 src={props.src}
---
+++ src/components/UIElements/ImagePlaceHolder.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ContentLoader from 'react-content-loader';
+import styled from "styled-components";
+
+
+const StyledImagePlaceHolder = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const StyledContentLoader = styled(ContentLoader)`
+    min-width: 100%;
+    min-height: 100%;
+`
+
+const StyledParagraph = styled.p`
+    position: absolute;
+`
+
+const ImagePlaceHolder = (props) => <StyledImagePlaceHolder>
+    <StyledContentLoader title={props.isErrorFound && "Error Loading"} animate={props.isErrorFound} speed={5}>
+        <rect x="0" y="0" width="100%" height="100%" />
+    </StyledContentLoader>
+    <StyledParagraph>Could not load image</StyledParagraph>
+</StyledImagePlaceHolder>;
+
+export default ImagePlaceHolder
