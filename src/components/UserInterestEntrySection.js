cf140b15af902e56db2301010580c3d10c5fa49b
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 19:28:40 +0300

    UserInterestEntrySection: remove redundant component

--- src/components/UserInterestEntrySection.js
+++ src/components/UserInterestEntrySection.js
@@ -1,7 +1,4 @@
-import React from "react";
 import UserInterestEntry from "./UIElements/UserInterestEntry";
 
-
-const UserInterestEntrySection = () => <UserInterestEntry />;
 
 export default UserInterestEntry
