f7feb4e406a640c3c6535f1f223ba584b00d2ead
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-14 20:23:55 +0300

    signUp: convert to styled components

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,7 +8,16 @@ import { FormInput } from "shards-react";
 import AnchorButton from "./UIElements/AnchorButton";
 import ButtonSuccess from "./UIElements/ButtonSuccess";
 import ScreenSizeContext from "./Contexts/ScreenSizeContext";
+import styled from "styled-components";
 
+
+const StyledDisplayFlexDiv = styled.div`
+    display: flex;
+`
+
+const StyledFlex1Div = styled.div`
+    flex: 1;
+`
 
 const SignUp = ({ email, first_name, last_name, username, google_id, facebook_id, twitter_id, github_id, linkedin_id, telegram_id }) => {
     const isDesktopOrLaptop = React.useContext(ScreenSizeContext).isDesktopOrLaptop;
@@ -30,52 +39,39 @@ const SignUp = ({ email, first_name, last_name, username, google_id, facebook_id
         <form method="post" onSubmit={event => {
             handleSubmit(event);
         }}>
-            <div style={{ display: 'flex', flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
-                <div style={{ flex: 1 }}>
+            <StyledDisplayFlexDiv style={{ flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
+                <StyledFlex1Div>
                     <Margin horizontal="0.5em" vertical="0.25em">
                         <ButtonSignInWith url={`${authEndpoint}/twitter`} borderColor={"#49a0e9"} backgroundColor={"#fff"} color={"#49a0e9"} imgSrc={`https://abs.twimg.com/favicons/twitter.ico`} value="Fill with Twitter" />
-
                         <ButtonSignInWith url={`${authEndpoint}/facebook`} borderColor={"#3b5998"} backgroundColor={"#3b5998"} color={"#fff"} imgSrc={`https://upload.wikimedia.org/wikipedia/commons/4/4d/F_icon_reversed.svg`} value="Fill with Facebook" />
-
                         <ButtonSignInWith url={`${authEndpoint}/google`} borderColor={"#eee"} backgroundColor={"#fff"} color={"#000"} imgSrc={`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_"G"_Logo.svg`} value="Fill with Google" />
-
                         <ButtonSignInWith url={`${authEndpoint}/github`} borderColor={"#24292e"} backgroundColor={"#fff"} color={"#24292e"} imgSrc={`https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg`} value="Fill with GitHub" />
-
                         <ButtonSignInWith url={`${authEndpoint}/linkedin`} borderColor={"#3577b5"} backgroundColor={"#3577b5"} color={"#fff"} imgSrc={`https://static-exp1.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico`} value="Fill with LinkedIn" />
                     </Margin>
-                </div>
-                <div style={{ flex: 1 }}>
+                </StyledFlex1Div>
+                <StyledFlex1Div>
                     <div>
                         Email
-              <FormInput name="email" value={user.email} onChange={handleUpdate} />
+                        <FormInput name="email" value={user.email} onChange={handleUpdate} />
                     </div>
-
-
                     <div>
                         First Name
-              <FormInput name="first_name" value={user.first_name} onChange={handleUpdate} />
+                        <FormInput name="first_name" value={user.first_name} onChange={handleUpdate} />
                     </div>
-
-
                     <div>
                         Last Name
-              <FormInput name="last_name" value={user.last_name} onChange={handleUpdate} />
+                        <FormInput name="last_name" value={user.last_name} onChange={handleUpdate} />
                     </div>
-
-
                     <div>
                         Username
-              <FormInput name="username" value={user.username} onChange={handleUpdate} />
+                        <FormInput name="username" value={user.username} onChange={handleUpdate} />
                     </div>
-
-
                     <div>
                         Password
-              <FormInput type="password" name="password" onChange={handleUpdate} />
+                        <FormInput type="password" name="password" onChange={handleUpdate} />
                     </div>
-
-                </div>
-            </div>
+                </StyledFlex1Div>
+            </StyledDisplayFlexDiv>
 
 
             <Margin vertical="1em" right="1em">
