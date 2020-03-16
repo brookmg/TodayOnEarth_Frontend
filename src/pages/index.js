331ea98ed6cab2012eb1094f00f0d47172c749dd
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-16 11:56:37 +0300

    home: change to posts from providers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import TrendingKeywords from "../components/TrendingKeywords";
-import PostsTrendingToday from "../components/PostsTrendingToday";
-import styled from "styled-components";
+import PostsFromUserProvider from "../components/PostsFromUserProvider";
 
-
-const StyledDiv = styled.div`
-    flex: 1;
-    padding-top: 15em;
-    height: 100vh;
-    overflow: auto;
-`
 
 const IndexPage = () => {
     return (
         <Layout
-            rightSideDesktopComponent={(
-                <StyledDiv>
-                    <TrendingKeywords />
-                </StyledDiv>
-            )}
             render={
                 ({ scrollValue, height }) => {
                     return (
                         <>
                             <SEO title="Home" />
-                            <PostsTrendingToday scrollValue={scrollValue} height={height} />
+                            <PostsFromUserProvider scrollValue={scrollValue} height={height} />
                         </>
                     )
                 }}>
