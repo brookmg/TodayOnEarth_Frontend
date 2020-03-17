2c70c69d827e86b76d61b165c425a4d8b269ef36
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-17 19:49:18 +0300

    home: dont show suggestion if loading

--- src/components/PostsFromUserProvider.js
+++ src/components/PostsFromUserProvider.js
@@ -131,7 +131,7 @@ export const PostsFromUserProvider = ({ scrollValue, height }) => {
                 posts.map(p => <PostHolderCard showRelevance={true} key={p.source_link} id={p.postid} title={ellipsedSubstring(p.title, 200)} body={p.body} sourceLink={p.source_link} imgSrc={getIfAvailable(p, 'metadata.message.image.src') || // Telegram images
                     getIfAvailable(p, 'metadata.post.thumbnail_image') // Instagram images
                 } metadata={p.metadata} />)}
-            {posts.length === 0 && <StyledP>No posts found, try adding more content sources ( Settings > Content Sources ) </StyledP>}
+            {(!loading && posts.length === 0) && <StyledP>No posts found, try adding more content sources ( Settings > Content Sources ) </StyledP>}
             {loading && <p>Loading Posts...</p>}
             {error && <p>Error: ${error.message}</p>}
             {!hasMorePosts &&
