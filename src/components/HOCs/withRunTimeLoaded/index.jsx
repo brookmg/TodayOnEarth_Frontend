efb169b08a0e892188c261c9eed74ed3c6762cef
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-03-19 12:36:46 +0300

    ui: add HOC for loading pages during runtime

---
+++ src/components/HOCs/withRunTimeLoaded/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "../../Image";
+import { StyledDiv } from "./styles";
+import HappyGuyGIF from "../../../images/happy-guy.gif";
+
+
+const withRunTimeLoaded = ComponentToWrap => props => {
+
+    const [isMounted, setIsMounted] = React.useState(false);
+    React.useEffect(() => {
+        setIsMounted(false)
+    }, [])
+
+    return (
+        <>
+            {
+                isMounted ?
+                    <ComponentToWrap {...props} />
+                    :
+                    <StyledDiv>
+                        <Image src={HappyGuyGIF} />
+                        <p>Loading page ...</p>
+                    </StyledDiv>
+
+            }
+        </>
+    )
+}
+
+export default withRunTimeLoaded;
