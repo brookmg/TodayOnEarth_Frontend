8fcf61648d80507df404decd9e3f5acb2d0c8dbf
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-04-18 16:02:11 +0300

    passwordReset: add new password entry

---
+++ src/components/PasswordReset/styles.js
@@ -0,0 +1,12 @@
+/**
+ * This file contains styles for PasswordReset component
+ */
+import styled from "styled-components";
+
+
+export const StyledCenteredDiv = styled.div`
+    max-width: 20em;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+`;
